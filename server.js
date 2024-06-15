651bd71f9c791da9b8da2030646e96d3caeb6a13
Vishwa S <136370992+VishwaSankar@users.noreply.github.com>
2024-06-15 20:07:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const connect= async ()=> {
 app.use(sessionMiddleware);
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin:"http://localhost:3000",credentials:true}))
+app.use(cors({origin:"https://gaminghaven.onrender.com",credentials:true}))
 app.use("/auth",authRoute)
 app.use("/users",userRoute)
 app.use("/fav",favroute)
